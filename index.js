268e7f869d7cc2c90ec112101c1c81bfb07ca023
huanggengzhong <915835528@qq.com>
2021-01-14 22:33:20 +0800

    feat:开始action

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 #!/usr/bin/env node
  // 上面是hashbang固定写法
-
-
 const {
   program
 } = require('commander');
 
+const helpOptions = require("./lib/core/help")
+
+const createCommands = require("./lib/core/create")
+// 导入版本
 program.version(require('./package.json').version);
 
-program.parse(process.argv) //显示版本号
+// 帮助选项
+helpOptions();
+// 创建指令
+createCommands();
+
+//显示版本号
+program.parse(process.argv)
+
+
+
+
+// console.log(program.dest);
